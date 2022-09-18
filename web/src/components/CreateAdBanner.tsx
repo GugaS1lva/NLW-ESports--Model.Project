1e854f7d5a2c9da4a81954a87d4afa8097538d2e
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-09-18 01:33:16 -0300

    [Server + Web]
    Consumindo a API + Modal pra inserção de um novo anúncio.

---
+++ web/src/components/CreateAdBanner.tsx
@@ -0,0 +1,21 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
+import * as Dialog from '@radix-ui/react-dialog'
+
+
+export function CreateAdBanner() {
+    return (
+        <div className='bg-nlw-gradient mt-8 self-stretch rounded-lg overflow-hidden'>
+            <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+                <div>
+                    <strong className='text-white text-2xl font-black block'>Não encontrou seu duo?</strong>
+                    <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+                </div>
+
+                <Dialog.Trigger className='px-4 py-3 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3'>
+                    <MagnifyingGlassPlus size={24} />
+                    Publicar anúncio
+                </Dialog.Trigger>
+            </div>
+        </div>
+    )
+}
